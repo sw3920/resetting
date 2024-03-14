9c89b80810f9c1bcd3e55a26878f87477282848c
sw3920 <163467618+sw3920@users.noreply.github.com>
2024-03-14 22:25:43 +0000

    Update reset.py

--- reset.py
+++ reset.py
@@ -5,11 +5,10 @@ dt = 1/10000
 w=0
 D = 0.5
 K = 0.7393
-k = ((((K)/x_0)**2)*D)*2
+k = ((((K)/x_0)**2)*D)
 epsilon = 0
 count = 1000
 r_vals = np.arange(0,10,1)
-print(r_vals)
 mfpt_vals = []
 for r in r_vals:
     mfpt = 0
@@ -20,7 +19,7 @@ for r in r_vals:
             if np.random.rand()<r*dt :
                 x = x_0
             else:
-                dx = (-k+epsilon*np.sin(w*T))*(x)*dt + np.sqrt(2*D*dt)*np.random.normal(loc=0, scale=1)
+                dx = 2*(-k+epsilon*np.sin(w*T))*(x)*dt + np.sqrt(2*D*dt)*np.random.normal(loc=0, scale=1)
                 x = x + dx
             T=T+dt
         mfpt = mfpt + T/count

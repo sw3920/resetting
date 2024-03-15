00abaa5c7982a0b300acc74571174c92aa6f45e0
sw3920 <163467618+sw3920@users.noreply.github.com>
2024-03-15 00:26:39 +0000

    Update jobs.sh

--- jobs.sh
+++ jobs.sh
@@ -7,6 +7,6 @@ module load SciPy-bundle/2022.05-foss-2022a
 
 cp $HOME/resetting/reset.py $TMPDIR
 
-python3 reset.py > output.txt
+python reset.py > output.txt
 
 cp $TMPDIR/output.txt $HOME/resetting/
